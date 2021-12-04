5a95883ccc2cee982eb03a5c97ba18b989f3be77
levgordin <levgordin@gmail.com>
2021-12-04 16:16:56 +0200

    create node and ts  hello world app

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+var num1 = 2;
+var num2 = 4;
+var sum = num1 + num2;
+console.log(sum);
--- file.js
+++
@@ -1,3 +0,0 @@
-console.log("hello world");
-console.log("will it work now?");
-console.log("now lets v work");
